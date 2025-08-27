c077b4c77804759c5a61fdc9d2daacd69eec3f46
inilogicz <inioluwanimi4sure@gmail.com>
2025-08-27 08:43:48 +0100

    feat: fixed tailwind confflict

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,21 @@
-// File: tailwind.config.mjs
+// // File: tailwind.config.mjs
 
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-montserrat)', 'sans-serif'],
-        nanum: ['var(--font-nanum-myeongjo)', 'serif'],
-      },
-    },
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// const config = {
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['var(--font-montserrat)', 'sans-serif'],
+//         nanum: ['var(--font-nanum-myeongjo)', 'serif'],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
 
-export default config;
+// export default config;
